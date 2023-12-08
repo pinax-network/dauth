5c230a53d2ba8bf0ff115d423dbb43386a89c346
Frederik Schöll <fschoell@posteo.de>
2023-12-08 14:39:06 +0100

    add some logging

--- middleware/grpc/authentication.go
+++ middleware/grpc/authentication.go
@@ -3,6 +3,8 @@ package server
 import (
 	"context"
 	"github.com/streamingfast/dtracing"
+	"github.com/streamingfast/logging"
+	"go.uber.org/zap"
 	"net/url"
 	"regexp"
 
@@ -11,6 +13,8 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/metadata"
 )
+
+var zlog, _ = logging.PackageLogger("dauth", "github.com/streamingfast/dauth/middleware/grpc")
 
 var portSuffixRegex = regexp.MustCompile(`:[0-9]{2,5}$`)
 var EmptyMetadata = metadata.New(nil)
@@ -30,7 +34,10 @@ func validateAuth(ctx context.Context, path string, authenticator dauth.Authenti
 		md = EmptyMetadata
 	}
 
-	md["SF_TRACE_ID"] = []string{dtracing.GetTraceIDOrEmpty(ctx).String()}
+	traceId := dtracing.GetTraceIDOrEmpty(ctx).String()
+	zlog.Info("validate auth called", zap.String("trace_id", traceId))
+
+	md["SF_TRACE_ID"] = []string{traceId}
 
 	ctx, err := authenticator.Authenticate(ctx, path, url.Values(md), middleware.RealIP(peerFromContext(ctx), md))
 	if err != nil {
